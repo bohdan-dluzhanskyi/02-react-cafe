1d4d142998474fc2e944199e1eca61ca59f6aef7
Bohdan_Dluzhanskyi <kvant0109@gmail.com>
2025-11-07 22:57:10 +0200

    Add-declarations

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import 'modern-normalize';
 import './index.css';
-import App from './components/App/App.tsx'; // 👈 правильний шлях до App
+import App from './components/App/App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
